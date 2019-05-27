33c77a4c370c4c2e89db9fb42b83043e695c081e
Alberto Diarian Sancho <aldiarian@gmail.com>
2019-05-27 20:35:48 +0200

    cambiar usuarios

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -11,7 +11,7 @@ export class MyApp extends LitElement {
         };
     }
 
-    constructor(){
+    constructor() {
         super();
         this.alumnos = [];
         this.db = firebase.firestore();
@@ -32,39 +32,62 @@ export class MyApp extends LitElement {
         `;
     }
     render() {
-        return html`
+        return html `
         <my-input @newInput=${this._userInsert}></my-input>
-        <my-list .alumnos=${this.alumnos}></my-list>
+        <my-list .alumnos=${this.alumnos} @deleteAlumno=${this._borrarAlumno} @cambiadoAlumno=${this._changeAlumno}></my-list>
         `;
     }
 
-    _userInsert(event){
+    _userInsert(event) {
         this.db.collection("alumnos").add({
-            nombre: event.detail,
-			pagado: false,
-			altaFecha: this.fechaAlta.getFullYear()
-        })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
+                nombre: event.detail,
+                pagado: false,
+                altaFecha: this.fechaAlta.getFullYear()
+            })
+            .then(function() {
+                console.log("Document successfully written!");
+            })
+            .catch(function(error) {
+                console.error("Error writing document: ", error);
+            });
 
         console.log(this.alumnos);
     }
-    _getUsers(){
-        this.db.collection("alumnos").onSnapshot( querySnapshot =>  {
-                const items = [];
-                querySnapshot.forEach( doc =>  {
-                    let currentuser = doc.data();
-                    currentuser.id = doc.id;
-                    items.push( currentuser ) ;
-                });
-                this.alumnos = items;
-                console.log(this.alumnos);
+    _getUsers() {
+        this.db.collection("alumnos").onSnapshot(querySnapshot => {
+            const items = [];
+            querySnapshot.forEach(doc => {
+                let currentuser = doc.data();
+                currentuser.id = doc.id;
+                items.push(currentuser);
             });
-        
+            this.alumnos = items;
+            console.log(this.alumnos);
+        });
+
+    }
+    _borrarAlumno(event) {
+        console.log(event.detail);
+        this.db.collection("alumnos").doc(event.detail).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    }
+
+    _changeAlumno(event) {
+        console.log('usuario cambiado', event.detail.id);
+        const usuario = event.detail;
+        const userRef = this.db.collection("alumnos").doc(usuario.id);
+        return userRef.update(usuario)
+            .then(() => {
+                console.log(' actualizado con exito');
+            })
+            .catch((error) => {
+                console.log(error);
+
+            })
+
     }
 }
 customElements.define('my-app', MyApp);
--- src/components/my-list.js
+++ src/components/my-list.js
@@ -1,4 +1,5 @@
-import { LitElement, html ,css } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import './my-alumno';
 
 class MyList extends LitElement {
     static get properties() {
@@ -7,7 +8,7 @@ class MyList extends LitElement {
         };
     }
 
-    constructor(){
+    constructor() {
         super();
         this.alumnos = [];
     }
@@ -25,49 +26,20 @@ class MyList extends LitElement {
             padding:0;
             font-family: sans-serif
         }
-        .alumno-item{
-            margin-bottom: 1rem;
-            display: flex;
-            justify-content: space-between;
-        }
-        .editar{
-            display: flex;
-        }
-        .editar-icon{
-            cursor: pointer;
-        }
-        svg{
-            pointer-events:none;
-        }
         `;
     }
 
     render() {
-        return html`
+            return html `
         <p>Lista de alumnos</p>
         <ul>
-        ${this.alumnos.map( element => html`
-            <li class="alumno-item">
-                <div>
-                    <div>Nombre: ${element.nombre}</div>
-                    <div>Fecha de alta: ${element.altaFecha}</div>
-                    <div>Pagado: ${element.pagado}</div>
-                </div>
-                <div class="editar">
-                    <div class="editar-icon edit" @click="${this._edit}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                    </div>
-                    <div class="editar-icon delete" @click="${this._editDelete}"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                    </div>
-                </div>
-            </li> `)}
+            ${this.alumnos.map( element => html` 
+                <my-alumno .alumno=${element}></my-alumno>
+            `)}
         </ul>
         `;
     };
 
-    _editDelete(event){
-        console.log( this.alumnos);
-    }
+   
 }
 customElements.define('my-list', MyList);
